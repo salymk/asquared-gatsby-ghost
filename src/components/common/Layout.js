42c7fed472ea8800b80483eec7d8fb81f37e4267
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-11-02 13:17:11 -0700

    Added bulma styles

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,29 +1,43 @@
+/* eslint-disable */
+
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 import { Link, StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
+import { jsx } from 'emotion'
 
 import { Navigation } from '.'
 import config from '../../utils/siteConfig'
 
 // Styles
-import '../../styles/app.css'
+// import '../../styles/app.css'
 import '../../styles/styles.scss'
 
 /**
-* Main layout component
-*
-* The Layout component wraps around each page and template.
-* It also provides the header, footer as well as the main
-* styles, and meta data for each page.
-*
-*/
+ * Main layout component
+ *
+ * The Layout component wraps around each page and template.
+ * It also provides the header, footer as well as the main
+ * styles, and meta data for each page.
+ *
+ */
 const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
     const site = data.allGhostSettings.edges[0].node
-    const twitterUrl = site.twitter ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}` : null
-    const facebookUrl = site.facebook ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}` : null
+    const twitterUrl = site.twitter
+        ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}`
+        : null
+    const facebookUrl = site.facebook
+        ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}`
+        : null
     console.log(site)
+
+    const onClick = () => {
+        const toggle = document.querySelector(`.navbar-burger`);
+        const menu = document.querySelector(`.navbar-menu`);
+        toggle.classList.toggle(`is-active`);
+        menu.classList.toggle(`is-active`);
+    };
     return (
         <>
             <Helmet>
@@ -32,68 +46,98 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                 <body className={bodyClass} />
             </Helmet>
 
-            <div className="viewport">
-
-                <div className="viewport-top">
-                    {/* The main header section on top of the screen */}
-                    <header className="site-head" style={{ ...site.cover_image && { backgroundImage: `url(${site.cover_image})` } }}>
+            <div className="">
+                {/* The main header section on top of the screen */}
+                <header>
+                    <nav className="navbar has-shadow is-fixed-top">
                         <div className="container">
-                            <div className="site-mast">
-                                <div className="site-mast-left">
-                                    <Link to="/">
-                                        {site.logo ?
-                                            <img className="site-logo" src={site.logo} alt={site.title} />
-                                            : <Img fixed={data.file.childImageSharp.fixed} alt={site.title} />
-                                        }
+                            <div className="navbar-brand">
+                                <Link to="/" className="navbar-item">
+                                    {site.title}
+                                </Link>
+
+                                <a
+                                    className="navbar-burger"
+                                    aria-label="menu"
+                                    aria-expanded="false"
+                                    onClick={onClick}
+                                >
+                                    <span aria-hidden="true" />
+                                    <span aria-hidden="true" />
+                                    <span aria-hidden="true" />
+                                </a>
+                            </div>
+                            <div className="navbar-menu has-text-centered-mobile has-text-centered-tablet">
+                                <div className="navbar-end">
+                                    <Navigation
+                                        data={site.navigation}
+                                        navClass="navbar-item"
+                                    />
+                                </div>
+                                <div>
+                                    <div className="navbar-item">
+                                        <Link
+                                            className="button is-link has-text-weight-bold is-size-5"
+                                            to="/newsletter/"
+                                        >
+                                            Subscribe
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                </header>
+
+                <main css={{padding: "3rem 0.7rem"}}>
+                    {/* All the main content gets inserted here, index.js, post.js */}
+                    {children}
+                </main>
+
+                {/* The footer at the very bottom of the screen */}
+                <footer className="footer">
+                    <div className="container">
+                        <div className="level">
+                            <div className="level-left">
+                                <div className="level-item">
+                                    <Link to="/" className="title is-4">
+                                        {site.title}
                                     </Link>
                                 </div>
-                                <div className="site-mast-right">
-                                    { site.twitter && <a href={ twitterUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /></a>}
-                                    { site.facebook && <a href={ facebookUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /></a>}
-                                    <a className="site-nav-item" href={ `https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/` } target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a>
-                                </div>
                             </div>
-                            { isHome ?
-                                <div className="site-banner">
-                                    <h1 className="site-banner-title">{site.title}</h1>
-                                    <p className="site-banner-desc">{site.description}</p>
-                                </div> :
-                                null}
-                            <nav className="site-nav">
-                                <div className="site-nav-left">
-                                    {/* The navigation items as setup in Ghost */}
-                                    <Navigation data={site.navigation} navClass="site-nav-item" />
-                                </div>
-                                <div className="site-nav-right">
-                                    <Link className="site-nav-button" to="/about">About</Link>
-                                </div>
-                            </nav>
-                        </div>
-                    </header>
-
-                    <main className="site-main">
-                        {/* All the main content gets inserted here, index.js, post.js */}
-                        {children}
-                    </main>
-
-                </div>
-
-                <div className="viewport-bottom">
-                    {/* The footer at the very bottom of the screen */}
-                    <footer className="site-foot">
-                        <div className="site-foot-nav container">
-                            <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link> © 2019 &mdash; Published with <a className="site-foot-nav-item" href="https://ghost.org" target="_blank" rel="noopener noreferrer">Ghost</a>
-                            </div>
-                            <div className="site-foot-nav-right">
-                                <Navigation data={site.navigation} navClass="site-foot-nav-item" />
+                            <div className="level-right">
+                                <Navigation
+                                    data={site.navigation}
+                                    navClass="level-item"
+                                />
                             </div>
                         </div>
-                    </footer>
-
-                </div>
+                        <hr />
+                        <div className="columns">
+                            <div className="column">
+                                <h4 className="title is-4">
+                                    Follow me on social media
+                                </h4>
+                                <div className="buttons">
+                                    <a
+                                        className="button"
+                                        href={twitterUrl}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        {/* <img src={Twitter} alt="Icon" /> */}
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="column has-text-centered has-text-right-tablet">
+                                <p className="subtitle is-6">
+                                    © 2020 ASquared. All right reserved.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </div>
-
         </>
     )
 }
@@ -108,7 +152,7 @@ DefaultLayout.propTypes = {
     }).isRequired,
 }
 
-const DefaultLayoutSettingsQuery = props => (
+const DefaultLayoutSettingsQuery = (props) => (
     <StaticQuery
         query={graphql`
             query GhostSettings {
@@ -119,7 +163,7 @@ const DefaultLayoutSettingsQuery = props => (
                         }
                     }
                 }
-                file(relativePath: {eq: "ghost-icon.png"}) {
+                file(relativePath: { eq: "ghost-icon.png" }) {
                     childImageSharp {
                         fixed(width: 30, height: 30) {
                             ...GatsbyImageSharpFixed
@@ -128,7 +172,7 @@ const DefaultLayoutSettingsQuery = props => (
                 }
             }
         `}
-        render={data => <DefaultLayout data={data} {...props} />}
+        render={(data) => <DefaultLayout data={data} {...props} />}
     />
 )
 
