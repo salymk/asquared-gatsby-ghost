259f5658b881ee0ceb2b19f083f0f1c5a33f2f4d
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-11-02 13:18:25 -0700

    Created new page for articles

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,13 +1,56 @@
+/* eslint-disable */
+import { graphql } from 'gatsby'
 import React from 'react'
-import { Layout, PostCard, Pagination } from '../components/common'
+import PropTypes from 'prop-types'
+import { Layout, PostCard } from '../components/common'
+import { MetaData } from '../components/common/meta'
 
 
-const Articles = () => {
+const Articles = ({data, location}) => {
+    const posts = data.allGhostPost.edges
+    console.log(posts)
     return (
-        <Layout>
-            <h1>Articles</h1>
-        </Layout>
+        <>
+            <MetaData location={location} />
+
+            <Layout>
+                <h1 className='title is-1 has-text-centered has-text-weight-bold has-text-black-bis pt-6'>Articles</h1>
+                <hr className='cm-hr'/>
+                <div className='container pt-6'>
+                    <section className='post-feed'>
+                        {posts.map(({node}) => (
+                            <PostCard key={node.id} post={node}/>
+                        ))}
+                    </section>
+                </div>
+            </Layout>
+        </>
     )
 }
 
+Articles.propTypes = {
+    data: PropTypes.shape({
+        allGhostPost: PropTypes.object.isRequired,
+    }).isRequired,
+    location: PropTypes.shape({
+        pathname: PropTypes.string.isRequired,
+    }).isRequired,
+    pageContext: PropTypes.object,
+}
+
 export default Articles
+
+export const allPosts = graphql`
+query {
+    allGhostPost(
+        sort: { order: DESC, fields: [published_at] } 
+        
+        ) {
+      edges {
+        node {
+          ...GhostPostFields
+        }
+      }
+    }
+  }
+`
