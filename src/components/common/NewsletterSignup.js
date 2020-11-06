fc8fdb9294ccdc39fa1bddaa4d383a3ede5c3c42
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-11-06 08:09:44 -0700

    Created newsletter component

---
+++ src/components/common/NewsletterSignup.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+import React from "react"
+
+const NewsletterSignup = props => {
+  return (
+    <section className={`section nl-signup has-background-link ${props.className}`}>
+      <div className="container">
+        <div className="columns is-vcentered">
+          <div className="column is-6 has-text-centered">
+            <h3 className="title has-text-white ">{props.title}</h3>
+            <p className="subtitle has-text-white">{props.subtitle}</p>
+          </div>
+          <div className="column is-5 ">
+            <div className="field has-addons">
+              <div className="control is-expanded">
+                <input
+                  className="input"
+                  type="email"
+                  placeholder={props.placeholder}
+                  aria-label="email"
+                />
+              </div>
+              <div className="control">
+                <button className="button is-black">{props.cta}</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default NewsletterSignup
