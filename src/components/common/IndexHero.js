31bc70e7248cf6cc3364134163fa9ddee8e91f46
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-11-26 12:12:34 -0700

    Changed padding for hero-body

--- src/components/common/IndexHero.js
+++ src/components/common/IndexHero.js
@@ -1,19 +1,20 @@
 /* eslint-disable */ 
 import React from "react"
 import { Link } from "gatsby"
+import { jsx } from 'emotion'
 
 const IndexHero = props => {
   return (
     <section className="hero py-6">
-      <div className="hero-body">
+      <div className="hero-body" css={{padding: "2.5rem .5rem"}}>
         <div className="container">
           <div className="columns is-centered">
             <div className="column is-12 is-10-desktop has-text-centered">
-              <h1 className="hero-title page-title-font mb-4 has-text-weight-bold is-capitalized has-text-black is-size-2-mobile">
+              <h1 className="hero-title page-title-font mb-4 has-text-weight-bold is-capitalized has-text-black is-size-3-mobile">
                 Do things <span className="has-text-link">right</span>.<br /> Do
                 things <span className="has-text-link">better</span>.
               </h1>
-              <p className="subtitle is-4 py-3">{props.subtitle}</p>
+              <p className="subtitle is-4 is-size-4-mobile py-3">{props.subtitle}</p>
               <div className="buttons is-centered">
                 <Link
                   to="/start-here/"
