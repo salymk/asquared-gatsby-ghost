234cdc9f1ad00f635ec8e88a1e3504d5daa4a6b5
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2020-12-02 18:17:03 -0700

    Changed font size

--- src/components/common/NewsletterSignup.js
+++ src/components/common/NewsletterSignup.js
@@ -1,34 +1,43 @@
 /* eslint-disable */
-import React from "react"
+import React from 'react';
 
-const NewsletterSignup = props => {
-  return (
-    <section className={`section nl-signup has-background-link ${props.className}`}>
-      <div className="container">
-        <div className="columns is-vcentered">
-          <div className="column is-6 has-text-centered">
-            <h3 className="title has-text-white title-font">{props.title}</h3>
-            <p className="subtitle has-text-weight-normal has-text-white subtitle-font">{props.subtitle}</p>
-          </div>
-          <div className="column is-5 ">
-            <div className="field has-addons">
-              <div className="control is-expanded">
-                <input
-                  className="input"
-                  type="email"
-                  placeholder={props.placeholder}
-                  aria-label="email"
-                />
-              </div>
-              <div className="control">
-                <button className="button is-black">{props.cta}</button>
-              </div>
+export default function NewsletterSignup(props) {
+    return (
+        <section
+            className={`section nl-signup has-background-link ${props.className}`}
+        >
+            <div className="container">
+                <div className="columns is-vcentered">
+                    <div className="column is-7 has-text-centered">
+                        <h3 className="title is-size-4-mobile has-text-white ">
+                            {props.title}
+                        </h3>
+                        <p className="cm-subtitle has-text-weight-normal has-text-white">
+                            {props.subtitle}
+                        </p>
+                    </div>
+                    <div className="column is-5 ">
+                        <div className="field has-addons">
+                            <div className="control is-expanded">
+                                <input
+                                    className="input"
+                                    type="email"
+                                    placeholder={props.placeholder}
+                                    aria-label="email"
+                                />
+                            </div>
+                            <div className="control">
+                                <button
+                                    type="button"
+                                    className="button is-black"
+                                >
+                                    {props.cta}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+        </section>
+    )
 }
-
-export default NewsletterSignup
